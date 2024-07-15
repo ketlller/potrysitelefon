b844081afca52f5714025f9eb8c733c5cb7ae2f5
ketlller <oculusvrdk2@gmail.com>
2024-07-15 03:15:03 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+let balance = 0;
+let shakeCount = 0;
+
+if ('ondevicemotion' in window) {
+    window.addEventListener('devicemotion', function(event) {
+        const acceleration = event.acceleration;
+
+        if (acceleration.x > 15 || acceleration.y > 15 || acceleration.z > 15) {
+            shakeCount++;
+            
+            if (shakeCount >= 5) {
+                balance += 5;
+                document.getElementById('balance').innerText = `Баланс: ${balance} EOS`;
+                shakeCount = 0;
+            }
+        }
+    });
+} else {
+    document.getElementById('message').innerText = 'Ваше устройство не поддерживает обнаружение тряски.';
+}
