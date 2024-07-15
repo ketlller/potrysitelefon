330c92a86fbbc703dcf085fea59c2cc2a71712e1
ketlller <oculusvrdk2@gmail.com>
2024-07-15 12:16:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,11 +7,26 @@ const threshold = 20; // Увеличиваем порог чувствител�
 const shakeCooldown = 500; // Увеличиваем время между трясками до 2.5 секунд
 let currentLevel = 1;
 const levels = [
-    { maxCoins: 100, nextLevel: 2 },
-    { maxCoins: 500, nextLevel: 3 },
-    { maxCoins: 1000, nextLevel: 4 },
-    { maxCoins: 1500, nextLevel: 5 },
-    { maxCoins: 3000, nextLevel: 6 }
+    { maxCoins: 0.01, nextLevel: 2 },
+    { maxCoins: 0.05, nextLevel: 3 },
+    { maxCoins: 0.1, nextLevel: 4 },
+    { maxCoins: 0.5, nextLevel: 5 },
+    { maxCoins: 1, nextLevel: 6 },
+    { maxCoins: 1.5, nextLevel: 7 },
+    { maxCoins: 2, nextLevel: 8 },
+    { maxCoins: 2.5, nextLevel: 9 },
+    { maxCoins: 3, nextLevel: 10 },
+    { maxCoins: 5, nextLevel: 11 },
+    { maxCoins: 10, nextLevel: 12 },
+    { maxCoins: 20, nextLevel: 13 },
+    { maxCoins: 50, nextLevel: 14 },
+    { maxCoins: 100, nextLevel: 15 },
+    { maxCoins: 250, nextLevel: 16 },
+    { maxCoins: 500, nextLevel: 17 },
+    { maxCoins: 1000, nextLevel: 18 },
+    { maxCoins: 2000, nextLevel: 19 },
+    { maxCoins: 5000, nextLevel: 20 }
+    
 ];
 
 const coinSound = document.getElementById('coinSound');
@@ -31,18 +46,18 @@ function updateBalance() {
 }
 
 function updateProgressBar() {
-    const levelInfo = levels[currentLevel - 1];
+    const levelInfo = levels[currentLevel - 0.0001];
     const percentage = (balance / levelInfo.maxCoins) * 100;
     document.getElementById('progress-bar').style.width = `${percentage}%`;
 }
 
 function checkLevelUp() {
-    const levelInfo = levels[currentLevel - 1];
+    const levelInfo = levels[currentLevel - 0.0001];
     if (balance >= levelInfo.maxCoins) {
         if (currentLevel < levels.length) {
             currentLevel = levelInfo.nextLevel;
-            alert(`Поздравляем! Вы достигли уровня ${currentLevel}`);
             playSound(levelUpSound);
+            alert(`Поздравляем! Вы достигли уровня ${currentLevel}`);
             document.getElementById('message').innerText = `Трясите телефон для достижения ${levels[currentLevel - 1].maxCoins} EOS!`;
             updateProgressBar();
         } else {
