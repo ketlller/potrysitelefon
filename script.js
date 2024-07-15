d2e59ae3d985030942b429de3b7ed759a6eaacbc
ketlller <oculusvrdk2@gmail.com>
2024-07-15 03:35:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 let balance = 0;
 let shakeCount = 0;
 
-if ('ondevicemotion' in window) {
-    window.addEventListener('devicemotion', function(event) {
-        const acceleration = event.acceleration;
+function updateBalance() {
+    balance += 5;
+    document.getElementById('balance').innerText = `Баланс: ${balance} EOS`;
+}
 
-        if (acceleration.x > 15 || acceleration.y > 15 || acceleration.z > 15) {
-            shakeCount++;
-            
-            if (shakeCount >= 5) {
-                balance += 5;
-                document.getElementById('balance').innerText = `Баланс: ${balance} EOS`;
-                shakeCount = 0;  // Сбрасываем счетчик трясок после увеличения баланса
-            }
-        }
-    });
+function onShake() {
+    shakeCount++;
+    if (shakeCount >= 5) {
+        updateBalance();
+        shakeCount = 0;
+    }
+}
+
+if (typeof Shake !== 'undefined') {
+    const shakeEvent = new Shake({ threshold: 15 });
+    shakeEvent.start();
+
+    window.addEventListener('shake', onShake, false);
 } else {
     document.getElementById('message').innerText = 'Ваше устройство не поддерживает обнаружение тряски.';
 }
